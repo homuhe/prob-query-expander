3aa2c6abe1e68247a465ecd9953002eb379256f2
homuhe <homuhe@gmail.com>
2017-03-01 15:22:53 +0100

    test1 commit

--- src/main/scala/com/ir/App.scala
+++ src/main/scala/com/ir/App.scala
@@ -8,7 +8,7 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello Wor" +
+    println( "Hello Wor Test1" +
       "vcxvxcld!" )
     println("concat arguments = " + foo(args))
   }
