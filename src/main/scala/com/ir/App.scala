1e0f33ffc1c871a38f4ff9265dce72b7361a9137
homuhe <homuhe@gmail.com>
2017-03-01 15:14:59 +0100

    revert

--- src/main/scala/com/ir/App.scala
+++ src/main/scala/com/ir/App.scala
@@ -8,7 +8,8 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    println( "Hello Wor" +
+      "vcxvxcld!" )
     println("concat arguments = " + foo(args))
   }
 
