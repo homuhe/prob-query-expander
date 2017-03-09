a7edd2b344be6134fe4dac50cbdaf0c07a4bf407
homuhe <homuhe@gmail.com>
2017-03-09 18:08:46 +0100

    clean up phraseExtractor

--- src/main/scala/com/ir/PhraseExtractor.scala
+++ src/main/scala/com/ir/PhraseExtractor.scala
@@ -9,21 +9,26 @@ import java.util.regex.Pattern
 class PhraseExtractor {
   /**
     * extracts a word array of a conllu format file that contains only words (no punctuation) and is lowercased
-    * @param inputfile : a String that denotes the input file
+    * @param file : a String that denotes the input file
     * @return the array of words contained in that file
     */
-  def preprocessFile(inputfile: String): Array[String] = {
+  def preprocessing(file: String): Array[String] = {
     val delimiter = "[ \t\n\r,.?!\\-:;()\\[\\]'\"/*#&$]+"
-    val words = Source.fromFile(inputfile).getLines().filter(!_.isEmpty).map(_.split("\t")(1)).map(_.toLowerCase())
+
+    val words = Source.fromFile(file).getLines()
+                                          .filter(!_.isEmpty)
+                                          .map(_.split("\t")(1))
+                                          .map(_.toLowerCase())
     val spacePattern = Pattern.compile(delimiter)
-    val newwords = words.filter(el => spacePattern.matcher(el).find() == false).toArray
-    newwords
+
+    val processed_words = words.filter(!spacePattern.matcher(_).find()).toArray
+    processed_words
   }
 }
 object PhraseExtractor {
   def main(args: Array[String]): Unit = {
-    val file = "/home/neele/Dokumente/InformationRetrieval/query-hyb/testcorpus/53293.conll"
+    val file = "testcorpus/53293.conll"
     val f = new PhraseExtractor
-    f.preprocessFile(file)
+    f.preprocessing(file)
   }
 }
